9c392773bf5b9c15dfc206eacedd8bc1556f2876
thiagO031 <thiago.tsantana3@gmail.com>
2022-04-13 16:46:57 -0300

    Projeto Finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+let img = document.getElementById('imagem')
+
+function vermelho(){
+    img.src="./img/semaforo-vermelho (1).png"
+}
+
+function amarelo(){
+    img.src="./img/semaforo-amarelo (1).png"
+}
+
+function verde(){
+    img.src="./img/semaforo-verde (1).png"
+}
+
+function desligado(){
+    img.src="./img/semaforo-desligado (1).png"
+}
